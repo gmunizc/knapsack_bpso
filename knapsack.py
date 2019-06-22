04c374aff91c646520f4522dd0216de6773a5a40
Gabriel Muniz <futlouco7@gmail.com>
2019-06-22 18:09:51 -0300

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,8 +1,8 @@
 import argparse
+import pyswarms as ps
+import numpy as np
 
-
-def knapsack_bpso_solver(knapsack_max_weight, weights, values, n):
-    return "To Be Continued..."
+knapsack_dict = {}
 
 
 def knapsack_solver(W, wt, val, n):
@@ -13,7 +13,7 @@ def knapsack_solver(W, wt, val, n):
 
     # If weight of the nth item is more than Knapsack of capacity
     # W, then this item cannot be included in the optimal solution
-    if (wt[n - 1] > W):
+    if wt[n - 1] > W:
         return knapsack_solver(W, wt, val, n - 1)
 
         # return the maximum of two cases:
@@ -24,17 +24,51 @@ def knapsack_solver(W, wt, val, n):
                    knapsack_solver(W, wt, val, n - 1))
 
 
+def fitness_per_particle(mask):
+    value = 9999999999
+    weight = 0
+    for idx, bit in enumerate(mask):
+        value = value + bit * knapsack_dict["values"][idx]
+        weight = weight + bit * knapsack_dict["weights"][idx]
+
+    value = 1 / value
+
+    if weight > knapsack_dict["max_weight"]:
+        value = 9999999999
+
+    return value
+
+
+def fitness_function(x):
+    n_particles = x.shape[0]
+    j = [fitness_per_particle(x[i]) for i in range(n_particles)]
+    return np.array(j)
+
+
+def bpso_solution(best_pos):
+    sum_values = 0
+    for idx, bit in enumerate(best_pos):
+        sum_values += bit * knapsack_dict["values"][idx]
+
+    return sum_values
+
+
 def main(args):
-    values = eval(args.values)
-    weights = eval(args.weights)
-    knapsack_max_weight = args.max_weight
-    print("Values: {0}".format(values))
-    print("Weights: {0}".format(weights))
-    print("Max_weight: {0}".format(knapsack_max_weight))
-    n = len(values)
-    bpso_solution = knapsack_bpso_solver(knapsack_max_weight, weights, values, n)
-    non_bpso_solution = knapsack_solver(knapsack_max_weight, weights, values, n)
-    print("BPSO Solution: {0}".format(bpso_solution))
+    knapsack_dict["values"] = eval(args.values)
+    knapsack_dict["weights"] = eval(args.weights)
+    knapsack_dict["max_weight"] = args.max_weight
+
+    # Set-up hyperparameters
+    options = {'c1': args.c1, 'c2': args.c2, 'w': args.w, 'k': args.k, 'p': args.p}
+    # Call instance of PSO
+    dimensions = len(knapsack_dict["values"])  # dimensions should be the number of features
+    optimizer = ps.discrete.BinaryPSO(n_particles=args.n_particles, dimensions=dimensions, options=options)
+    # Perform optimization
+    best_cost, best_pos = optimizer.optimize(fitness_function, iters=args.iters)
+    non_bpso_solution = knapsack_solver(knapsack_dict["max_weight"], knapsack_dict["weights"],
+                                        knapsack_dict["values"], len(knapsack_dict["values"]))
+
+    print("BPSO Solution: {0}".format(bpso_solution(best_pos)))
     print("Non-BPSO Solution: {0}".format(non_bpso_solution))
 
 
@@ -45,6 +79,13 @@ if __name__ == '__main__':
     parser.add_argument('--weights', type=str, default="[10, 20, 30, 40, 50, 60]",
                         help='List containing the weight of each item.')
     parser.add_argument('--max_weight', type=int, default=100, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--c1', type=float, default=0.5, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--c2', type=float, default=0.3, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--w', type=float, default=0.9, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--k', type=int, default=15, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--p', type=int, default=1, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--n_particles', type=int, default=30, help='Maximum weight sustained by the knapsack.')
+    parser.add_argument('--iters', type=int, default=100, help='Maximum weight sustained by the knapsack.')
 
     args = parser.parse_args()
     main(args)
